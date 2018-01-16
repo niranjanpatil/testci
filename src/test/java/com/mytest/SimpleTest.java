2622ae24619713f5aca908cc4f5670437385c907
Niranjan Patil <niranjan@ooyala.com>
2018-01-16 00:14:48 -0600

    Adding my first test

---
+++ src/test/java/com/mytest/SimpleTest.java
@@ -0,0 +1,15 @@
+package com.mytest;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by niranjan on 1/16/18.
+ */
+public class SimpleTest {
+
+    @Test
+    public void myTest()
+    {
+        System.out.println("My first test using testNG !!");
+    }
+}
